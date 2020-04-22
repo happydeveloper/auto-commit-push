8d10260eb8d32ba27f451d9da8e0c18f6e7b26e6
duru <enfn2001@gmail.com>
2020-04-22 13:00:31 +0900

    M auto-commit-push.sh ?? auto-commit.sh ?? install.sh - 2020년 4월 22일 수요일 13시 00분 31초 KST

--- auto-commit-push.sh
+++ auto-commit-push.sh
@@ -7,13 +7,14 @@ echo "1 시간에 한번씩 자동으로 커밋하고 푸시함 "
 echo "변화가 없으면 커밋하지 않음"
 
 stt=$(git status)
+m="$(git status --short)"
 msg_date=$(date)
 git status
 
 echo "$?"
 if [[ "$stt" = *"수정함:"* ]]; then
   echo "********************변화가 있다"
-  git commit -am "$msg_date"
+  git commit -am "$m - $msg_date "
   git push origin master
   echo "$0"
   echo "*******************************"
