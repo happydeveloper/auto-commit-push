bb90ac0619b65cf1bcfdd474604bd8942dcfa7ff
duru <enfn2001@gmail.com>
2020-04-22 14:39:09 +0900

    M auto-commit.sh - 2020년 4월 22일 수요일 14시 39분 09초 KST

--- auto-commit.sh
+++ auto-commit.sh
@@ -15,3 +15,48 @@ if [ $# -ne 1 ]; then
 else
   watch -n $1 ./auto-commit-push.sh
 fi
+
+#For MacOSX, install coreutils (which includes greadlink)
+# $brew install coreutils
+if [ "${OSTYPE:0:6}" == "darwin" ]; then
+    READLINK="greadlink"
+else
+    READLINK="readlink"
+fi
+
+SHELL_HISTORY=~/.auto-commit_history
+DU_OPT="-q"
+BIN_DEPS="id $READLINK ls basename ls pwd cut"
+VERSION="0.2"
+
+umask 077
+
+#Dependencies check
+for i in $BIN_DEPS; do
+    which $i > /dev/null
+    if [ $? -ne 0 ]; then
+        echo -e "Error: Required program could not be found: $i"
+        exit 1
+    fi
+done
+
+
+#Looking for auto-commit
+if [ -f "./auto-commit.sh" ]; then
+    DU="./auto-commit.sh"
+else
+    DU=$(which auto-commit.sh)
+    if [ $? -ne 0 ]; then
+        echo "auto-commit Uploader not found!"
+        exit 1
+    fi
+fi
+
+#Check DropBox Uploader
+if [ ! -f "$DU" ]; then
+    echo "Dropbox Uploader not found: $DU"
+    echo "Please change the 'DU' variable according to the Dropbox Uploader location."
+    exit 1
+else
+    DU=$($READLINK -m "$DU")
+fi
