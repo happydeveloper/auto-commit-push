2260c842cfef58cb09405d427119113f5192271e
duru <enfn2001@gmail.com>
2020-04-22 13:27:33 +0900

    M auto-commit-push.sh - 2020년 4월 22일 수요일 13시 27분 33초 KST

--- auto-commit-push.sh
+++ auto-commit-push.sh
@@ -12,7 +12,7 @@ msg_date=$(date)
 git status
 git add .
 echo "$?"
-if [[ "$stt" = *"수정함:"* ]] || [[ "$stt" = *"새 파일:"* ]] || [["$stt" = *"삭제함:"*]]; then
+if [[ "$stt" = *"수정함:"* ]] || [[ "$stt" = *"새 파일:"* ]] || [["$stt" = *"삭제함:"* ]]; then
   echo "********************변화가 있다"
   git add .
   git commit -am "$m - $msg_date "
