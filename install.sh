8adde336664903f620c207b455bd99417dbbdbb9
duru <enfn2001@gmail.com>
2020-04-22 15:47:01 +0900

    M install.sh - 2020년 4월 22일 수요일 15시 47분 01초 KST

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
 install_app=$(pwd)/auto-commit.sh
+install_app_2=$(pwd)/auto-commit-push.sh
 echo $install_app
 ln -s $install_app /usr/local/bin/auto-commit
+ln -s $install_app_2 /usr/local/bin/auto-commit-push
