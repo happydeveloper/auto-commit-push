e09503d1c5a1f9200e9ea886e0551075093b5e52
duru <enfn2001@gmail.com>
2020-04-22 16:49:02 +0900

    M auto-commit-push.sh - diff --git a/auto-commit-push.sh b/auto-commit-push.sh index d42a743..e470f7d 100755 --- a/auto-commit-push.sh +++ b/auto-commit-push.sh @@ -6,13 +6,14 @@ echo "변화가 없으면 커밋하지 않음"  stt=$(git status)  m="$(git status --short)"  msg_date=$(date) +commit_msg="$(git diff)"  git status  git add .  echo "$?"  if [[ "$stt" = *"수정함:"* ]] || [[ "$stt" = *"새 파일:"* ]] || [[ "$stt" = *"삭제함:"* ]]; then    echo "********************변화가 있다********************"    git add . -  git commit -am "$m - $msg_date " +  git commit -am "$m - $commit_msg - $msg_date "    git push origin master    echo "$0"    echo "*************************************************" - 2020년 4월 22일 수요일 16시 49분 02초 KST

--- auto-commit-push.sh
+++ auto-commit-push.sh
@@ -6,13 +6,14 @@ echo "변화가 없으면 커밋하지 않음"
 stt=$(git status)
 m="$(git status --short)"
 msg_date=$(date)
+commit_msg="$(git diff)"
 git status
 git add .
 echo "$?"
 if [[ "$stt" = *"수정함:"* ]] || [[ "$stt" = *"새 파일:"* ]] || [[ "$stt" = *"삭제함:"* ]]; then
   echo "********************변화가 있다********************"
   git add .
-  git commit -am "$m - $msg_date "
+  git commit -am "$m - $commit_msg - $msg_date "
   git push origin master
   echo "$0"
   echo "*************************************************"
