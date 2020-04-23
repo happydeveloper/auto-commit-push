0d9e3861bd3fefea6f501a42fc0d1144474a9d9f
duru <enfn2001@gmail.com>
2020-04-23 17:26:54 +0900

    현재 브랜치 푸시

--- auto-commit-push.sh
+++ auto-commit-push.sh
@@ -10,14 +10,18 @@ commit_msg="$(git diff)"
 if [[ "$msg" = *"#cmsg"* ]]; then #cmsg 태그가 있으면 메시지를 grep해서 출력해줌
   commit_msg="$(git diff | grep "#cmsg")"
 fi
-git checkout -b logs
+
+git checkout -b logs #logs 브랜치 이동
+branch=$(cat .git/HEAD)
+
+
 git status
 git add .
 if [[ "$stt" = *"수정함:"* ]] || [[ "$stt" = *"새 파일:"* ]] || [[ "$stt" = *"삭제함:"* ]]; then
   echo "********************변화가 있다********************"
   git add .
   git commit -am "$m - $commit_msg - $msg_date "
-  git push origin master
+  git push origin ${branch#*ref: refs/heads/}
   echo "$0"
   echo "*************************************************"
 else 
